15e713ce0bd3d90f61a1121fea6cac7987a8246a
Ivan Grgurina <ivan.grgurina@gmail.com>
2014-06-01 15:24:35 +0200

    Update zadatak4.py

--- zadatak4.py
+++ zadatak4.py
@@ -1,27 +1,27 @@
-import re, urlib.request
+import re
+import urllib.request
 
-# koristim VS, pa mi se ne isplati ostvarivati preko argumenata naredbenog retka (to je ionako piece of cake)
-url = input("Enter the website address")
+# koristim Visual Studio, pa mi se ne isplati ostvarivati preko argumenata naredbenog retka
+# url = input("Enter the website address\n") or r'https://www.google.com'
 
-stranica = urlib.request.urlopen(url)
-mybytes = stranica.read()
-html = mybytes.decode("utf8")
-print html
+def load_url():
+	stranica = urllib.request.urlopen(input("Enter the website address\n"))
+	html = "".join(stranica.read().decode(encoding='UTF-8',errors='ignore'))
+	return html
 
-## 
-#  => označava opis problema kojeg fja rješava#  
+#html = load_url(url)
 
 ##
 # pronaci i izlistati sve linkove na druge stranice
 def find_urls(html):
-    regexp_link = r'''</?a((s+w+(s*=s*(?:".*?"|'.*?'|[^'">s]+))?)+s*|s*)/?>w+</a>'''
-    pattern = re.compile(regexp_link)
-
-	url_list = re.findall(pattern, html)
-
+	url_list = []
+	m = re.findall('href *= *[\'"]([^\'"]+)', html) # find the href
+	if m: url_list.extend(m)
 	return url_list
 
-print find_urls(html)
+print("All links on page: \n")
+for url in find_urls(html):
+	print(url)
 
 ##
 # napraviti listu svih hostova kojima se sa stranice može pristupiti (bez ponavljanja)
@@ -29,36 +29,46 @@ print find_urls(html)
 # za svaki host odrediti broj referenciranja u razmatranoj stranici
 def find_and_count_hosts(url_list):
 	host_list = {}
-	
 	for url in url_list:
-		# i.e. https://www.fer.unizg.hr/predmet/skrjez
-		host = url.split(r'//')[1].split(r'/')[0] # prvi split odvaja https, drugi odvaja host, tj domenu
-	
+		# https://www.fer.unizg.hr/predmet/skrjez
+		try:
+			pokusniKunic = str(url).split(r'/')[2]
+		except IndexError:
+			continue
+		host = str(url).split(r'/')[2]
 		if host in host_list:
-			host_list[host] = 1 # Python ne postavlja početno automatski u nulu kao što radi blaženi Perl
+			host_list[host] += 1
 		else:
-			host_list[host]++
-	
+			host_list[host] = 1
 	return host_list
+
+print("Domena\tBroj pojavljivanja\n")
+print(find_and_count_hosts(find_urls(html)))
 
 ##
 # pronaci sve e-mail adrese u toj stranici
 def find_emails(html):
+	# http://incurlybraces.com/python-extract-email-address-regular-expression.html
 	regexp_email = r'''([w-.+]+@w[w-]+.+[w-]+)'''
 	pattern = re.compile(regexp_email)
-
 	email_list = re.findall(pattern, html)
-
 	return email_list
 
 ##
 # prebrojati linkove na slike (<img src="url" ... >)
 def count_imgs(html):
-    img_count = 0
-    
-    for tag in re.findall('< *img +src[^>]+', html): # find the img tags
-        m = re.search('src *= *[\'"]([^\'"]+)', tag) # find the src
-        if m:
-            img_count++
-            
-    return img_count
+	img_count = 0
+	for tag in re.findall('< *img +src[^>]+', html): # find the img tags
+		m = re.search('src *= *[\'"]([^\'"]+)', tag) # find the src
+		if m:
+			# url_list.append(m.group(1)) # add the sub match
+			img_count += 1
+	return img_count
+
+def list_imgs(html):
+	img_list = []
+	for tag in re.findall('< *img +src[^>]+', html): # find the img tags
+		m = re.search('src *= *[\'"]([^\'"]+)', tag) # find the src
+		if m:
+			img_list.append(m.group(1)) # add the sub match
+	return img_list
