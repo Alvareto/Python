e50e409600134ea2e6852bd604a60d9dd1c23169
Ivan Grgurina <ivan.grgurina@gmail.com>
2014-06-03 19:21:29 +0200

    Create zadatak5.py

---
+++ zadatak5.py
@@ -0,0 +1,13 @@
+# Python 3.4
+import sys
+import collections
+
+count = collections.defaultdict(int)
+with open(sys.argv[1], "r") as dat:
+	datoteka = dat.readlines()
+	for line in datoteka:
+		ip = str(line.split()[0])		
+		count[ip] += 1
+
+for ip in sorted(count, key=count.get, reverse=True):
+	print(ip + " : "  + str(count[ip]))
